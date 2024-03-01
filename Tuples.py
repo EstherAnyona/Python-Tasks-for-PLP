457b33bcf7f2dc846ef7abaafd256bc56a48e085
Esther Murunga <144281904+EstherAnyona@users.noreply.github.com>
2024-03-01 09:54:30 +0300

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,9 @@
+#This is a program to create a tuple of favorite books and print them using a for loop
+
+favorite_books = ("Things Fall Apart", "Gifted Hands", "A Man of the People", "The River Between", "Caucasian Chalk Circle")
+
+print("My favorite books:")
+
+for book in favorite_books:
+    print (book)
+    
