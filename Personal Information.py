21782147e0480161465a85d01ee390b38735d4df
Esther Murunga <144281904+EstherAnyona@users.noreply.github.com>
2024-03-01 10:34:21 +0300

    Add files via upload

---
+++ Personal Information.py
@@ -0,0 +1,7 @@
+"This stores information regarding a person in a dictionary and stores it"
+
+person = {}
+person['name'] = input("Enter name: ")
+person['age'] = input("Enter age: ")
+person['favorite color'] = input("Enter favorite color: ")
+print("Person information:", person)
